23dd2cc0c85b766d29edb8b8adf0450ab27ad164
Vishal_Kashyap <vv9026472@gmail.com>
2024-08-23 19:36:38 +0530

    Scripts add for apache and nginx

---
+++ monitoring_scripts/apache.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt install apache2 -y
+
+sudo systemctl start apache2
+
+sudo systemctl enable apache2
+
+# Datadog Agent installation on Ubuntu Server.
+# Enter your Datadog API Key in below command.
+
+DD_API_KEY=YOUR_DATADOG_API_KEY DD_SITE="us5.datadoghq.com" bash -c "$(curl -L https://install.datadoghq.com/scripts/install_script_agent7.sh)"
+
+# Creating configuration file of datadog agent's apache's configuration file
+
+sudo bash -c 'cat <<EOL >> /etc/datadog-agent/conf.d/apache.d/conf.yaml
+
+init_config:
+
+instances:
+
+  - apache_status_url: http://localhost/server-status?auto
+
+logs:
+  - type: file
+    path: /var/log/apache2/access.log
+    source: apache
+    service: apache
+    sourcecategory: http_web_access
+
+  - type: file
+    path: /var/log/apache2/error.log
+    source: apache
+    service: apache
+    sourcecategory: http_web_error
+EOL'
+
+# Set permissions to access logs of apache
+
+sudo chmod -R 777 /var/log/apache2
+
+# Enable logs
+
+sudo sed -i 's/logs_enabled: false/logs_enabled: true/' /etc/datadog-agent/datadog.yaml
+
+# Restart Services
+
+sudo systemctl reload apache2
+sudo systemctl restart apache2
+sudo service datadog-agent restart
---
+++ monitoring_scripts/nginx.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt install nginx -y
+
+sudo systemctl start nginx
+
+sudo systemctl enable nginx
+
+# Enter your Datadog API Key in below command.
+
+DD_API_KEY=DATADOG_API_KEY DD_SITE="us5.datadoghq.com" bash -c "$(curl -L https://install.datadoghq.com/scripts/install_script_agent7.sh)"
+
+# Creating configuration file of datadog agent's nginx's configuration file
+
+sudo bash -c 'cat <<EOL >> /etc/datadog-agent/conf.d/nginx.d/conf.yaml
+
+server {
+    listen 80;
+    server_name localhost;
+
+    location /nginx_status {
+        stub_status on;
+        allow 127.0.0.1; # Only allow requests from localhost
+        deny all;        # Deny all other addresses
+    }
+}
+EOL'
+
+sudo bash -c 'cat <<EOL >> /etc/datadog-agent/conf.d/nginx.d/conf.yaml
+init_config:
+
+instances:
+  - nginx_status_url: http://localhost:80/nginx_status
+    tags:
+      - env:production
+      - app:webserver
+EOL'
+
+# Enable logs
+
+sudo sed -i 's/logs_enabled: false/logs_enabled: true/' /etc/datadog-agent/datadog.yaml
+
+# Restart Services
+
+sudo systemctl reload nginx
+sudo systemctl restart nginx
+sudo service datadog-agent restart
